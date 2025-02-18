a1c2a5eda17401465fba00d9c3f1e914c564f338
boosireddyprasadreddy7 <boosireddyprasadreddy@gmail.com>
2025-02-18 11:17:18 +0530

    Answer to Question which is asked only

---
+++ day2/even_number.py
@@ -0,0 +1,6 @@
+#check wheather number is even 
+input_number = int(input("Enter a number to check wheather it is even: "))
+if input_number % 2 == 0 :
+    print(f"{input_number} is an even number.")
+
+#answer to question which they asked not what you you like
