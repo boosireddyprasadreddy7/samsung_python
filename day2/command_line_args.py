f48417ed6a4af9452f35723928cfb67664a7e7b2
boosireddyprasadreddy7 <boosireddyprasadreddy@gmail.com>
2025-02-18 15:26:14 +0530

    Command line Arguments problem is added

---
+++ day2/command_line_args.py
@@ -0,0 +1,5 @@
+import sys 
+print(sys.argv)
+print(type(sys.argv))
+print(sys.argv[0])
+print(sys.argv[-1])
--- day2/smallest_biggest_number.py
+++ day2/smallest_biggest_number.py
@@ -1,12 +1,12 @@
+#You will given a number and you should find the smallest possible bigger number
+
 input_number = int(input("Enter the number to find next smallest bigger number: "))
 if len(str(input_number)) <= 2 :
     print("Not possible")
-ones_digit = input_number % 10 
-input_number = input_number // 10
-tens_digit = input_number % 10
-input_number = input_number // 10
-
-
-smallest_bigger_number = (input_number * 100) + (ones_digit * 10) + tens_digit
-
-print("Smallest Bigger Number: ", smallest_bigger_number)
+else: 
+    ones_digit = input_number % 10 
+    input_number = input_number // 10
+    tens_digit = input_number % 10
+    input_number = input_number // 10
+    smallest_bigger_number = (input_number * 100) + (ones_digit * 10) + tens_digit
+    print("Smallest Bigger Number: ", smallest_bigger_number)
