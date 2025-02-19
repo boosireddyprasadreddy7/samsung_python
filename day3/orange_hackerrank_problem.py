aec3e7ec2b931584edc05745f331c859789e5dcb
boosireddyprasadreddy7 <boosireddyprasadreddy@gmail.com>
2025-02-19 09:58:54 +0530

    orange partition problem solution is added

---
+++ day3/orange_hackerrank_problem.py
@@ -0,0 +1,11 @@
+number_of_oranges = int(input("Enter the number of oranges: "))
+list_of_oranges = list( map(int,input("Enter the list oranges size: ").split()))
+k = 0
+for i in range(0, len(list_of_oranges)-1):
+    if list_of_oranges[i] <= list_of_oranges[-1] :
+        list_of_oranges[i], list_of_oranges[k] = list_of_oranges[k], list_of_oranges[i]
+        k += 1
+
+list_of_oranges[-1], list_of_oranges[k] = list_of_oranges[k], list_of_oranges[-1]
+
+print(list_of_oranges)
